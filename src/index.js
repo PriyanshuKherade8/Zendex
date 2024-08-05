438fd590a1ddf1cb35d4c8b9d393e90064a2ead1
priyanshu.kherade <priyanshu.kherade@fyntune.com>
2024-08-05 18:21:10 +0530

    style: added scroll bar

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import MiniDrawer from "./modules/DashBoard";
+import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
